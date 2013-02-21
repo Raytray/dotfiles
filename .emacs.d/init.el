2b66fbe74acc47d1420fb11cb11fe7567a693f0d
Raymond Tang <Raytray25@gmail.com>
2013-02-20 21:43:09 -0500

    Revert "Added remove-dos-eol into emacs init."
    This reverts commit 8e167de3c987f719d0fd7a9b535addb23bdcc6d3.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -58,9 +58,3 @@
           (lambda()
             (setq sgml-basic-offset 4)
             (setq indent-tabs-mode t)))
-
-(defun remove-dos-eol ()
-  "Do not show ^M in files containing mixed UNIX and DOS line endings."
-  (interactive)
-  (setq buffer-display-table (make-display-table))
-  (aset buffer-display-table ?\^M []))
