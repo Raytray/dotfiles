6e2b37c7a96a0a854c9a2b9b4c4254c4cb5f9933
Raymond Tang <Raytray25@gmail.com>
2013-03-09 19:47:32 -0500

    Readded standard 4 spaces tabbing for html

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -43,3 +43,8 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(tab-stop-list (quote (4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120))))
+
+(add-hook 'html-mode-hook
+	  (lambda()
+            (setq sgml-basic-offset 4)
+            (setq indent-tabs-mode t)))
