ca611478a99634c151961581c633120c4b2a54d6
Raymond Tang <raymond@tang.me>
2013-04-25 10:00:13 -0400

    Added highlighting on trailing whitespaces/tabs.
    Also changed default pianobar config and added a few more ignores.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,7 +3,7 @@
 (add-to-list 'load-path "~/.emacs.d/plugins/")
 (add-to-list 'load-path "~/.emacs.d/plugins/deft")
 (add-to-list 'load-path "~/.emacs.d/plugins/markdown-mode")
-(setq auto-mode-alist 
+(setq auto-mode-alist
       (cons '("\\.md" . markdown-mode) auto-mode-alist)
       )
 (load "jinja")
@@ -23,7 +23,7 @@
     (let((count 0))
       (dolist(buffer (buffer-list))
         (set-buffer buffer)
-        (when (not (eq nil deft-note-mode))  
+        (when (not (eq nil deft-note-mode))
           (setq count (1+ count))
           (kill-buffer buffer)))
       )))
@@ -37,18 +37,26 @@
 (global-set-key (kbd "C-c C-v") 'recompile)
 
 ;; 4 spaces to a tab.
+(setq indent-tabs-mode nil)
 (setq-default c-basic-offset 4)
 (setq-default python-indent 4)
 (setq tab-width 4)
 (setq tab-stop-list (number-sequence 4 200 4))
-(setq indent-tabs-mode nil)    
 (add-hook 'html-mode-hook
         (lambda ()
           ;; Default indentation is usually 2 spaces, changing to 4.
           (set (make-local-variable 'sgml-basic-offset) 4)))
 
+;; Show whitespace
+(require 'highlight-chars)
+(add-hook 'font-lock-mode-hook 'hc-highlight-tabs)
+(add-hook 'change-major-mode-hook
+	  (lambda ()
+	    (add-hook 'font-lock-mode-hook
+		      'hc-highlight-trailing-whitespace)))
+
 (require 'package)
-(add-to-list 'package-archives 
+(add-to-list 'package-archives
     '("marmalade" .
       "http://marmalade-repo.org/packages/"))
 (package-initialize)
