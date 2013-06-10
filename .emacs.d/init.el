3943a5e517ce1a75c9d9f53a647d09d50b2d8982
Raymond Tang <raymond@tang.me>
2013-06-10 16:03:47 -0400

    Added multi-web-mode emacs plugin.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,6 +10,15 @@
 (load "markdown-mode")
 (load "git-commit")
 
+;;multi-web-mode
+(require 'multi-web-mode)
+(setq mweb-default-major-mode 'html-mode)
+(setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+                  (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
+                  (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
+(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
+(multi-web-global-mode 1)
+
 ;;deft
 (load "deft")
 (setq deft-extension "md")
