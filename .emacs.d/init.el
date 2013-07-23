712563eeeb17ea97d6794acdd8ff951c6530777c
Raymond Tang <raymond@parsely.com>
2013-07-23 09:51:09 -0400

    Partial revert of commit 763b479. Changing html spaces from 2 to 4 again.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -63,7 +63,7 @@
 (add-hook 'html-mode-hook
         (lambda ()
           ;; Default indentation is usually 2 spaces, changing to 4.
-          (set (make-local-variable 'sgml-basic-offset) 2)))
+          (set (make-local-variable 'sgml-basic-offset) 4)))
 
 ;; Show whitespace
 (require 'highlight-chars)
