c6cad79bd1ac24c3709b652ca2f60eb3b5337f5f
Raymond Tang <raymond@tang.me>
2016-07-28 19:20:21 -0400

    add emacs package manager

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -88,6 +88,11 @@
 (add-to-list 'package-archives
     '("marmalade" .
       "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
